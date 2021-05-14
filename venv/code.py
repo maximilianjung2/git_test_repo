da1f1a69990792260244c6068f2aa1e03c6cb78e
maximilianjung2 <maximilianjung@live.com>
2021-05-14 20:14:51 +0200

    first task

--- venv/code.py
+++ venv/code.py
@@ -1,2 +1,15 @@
+import datetime
 print('test')
 print('nächster commit')
+
+#programming task 1: https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+age=input('Wie alt wirst du dieses Jahr?')
+x=datetime.datetime.now()
+year=x.year
+int_age=int(age)
+int_goal=100
+int_diff=int_goal-int_age
+str_diff=str(int_diff)
+print('du wirst in ' + str_diff + ' Jahren 100')
+print('Das ist dann das Jahr' + str(year+int_diff))
+
