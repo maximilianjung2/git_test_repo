6113350a9889be96a25432eebb54c2876d363cb0
maximilianjung2 <maximilianjung@live.com>
2021-05-15 19:14:53 +0200

    3rd Exercise

---
+++ venv/Exercise3.py
@@ -0,0 +1,22 @@
+#https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
+list=[]
+result_list_positive=[]
+result_list_negative=[]
+x=0
+divisor=1
+list_anzahl=1
+list_anzahl=int(input('Wie viele Zahlen willst du prüfen?'))
+divisor= int(input('Gib den Divisor ein'))
+while x<4:
+    list.append(int(input('Gebe eine Zahl ein')))
+    x=x+1
+for element in list:
+    if element % divisor == 0:
+        result_list_positive.append(element)
+    else:
+        result_list_negative.append(element)
+print('teilbar durch: '+str(divisor))
+print(result_list_positive)
+print('nicht teilbar durch: '+str(divisor))
+print(result_list_negative)
+
